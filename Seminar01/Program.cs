de5f265cb19ac22de87048baf95ba2dd76ed6bb7
IrinaIaniv <yanividm@gmail.com>
2022-06-14 19:29:19 +0300

    create first seminar

--- Seminar01/Program.cs
+++ Seminar01/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* int num;
+
+Console.Write("input integer number ");
+num = Convert.ToInt32(Console.ReadLine());
+
+num = num * num;
+
+Console.WriteLine("Result is " + num);
+*/
+
+/* int num1, num2, quad;
+
+Console.Write("input first integer number ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input second integer number ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+quad = num2 * num2;
+
+if (num1 == quad)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+*/
+
+int num;
+
+Console.Write(" Input integer number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int current = num * (-1);
+
+while (current <= num)
+{
+    Console.Write(current + " ");
+    current++; // current + 1
+}
