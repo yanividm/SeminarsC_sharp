3e4c1c16f0ebca2232133e9afcb348dee255e493
IrinaIaniv <yanividm@gmail.com>
2022-06-23 05:53:32 +0300

    midresult of homework

---
+++ Seminar04_homework/Program.cs
@@ -0,0 +1,42 @@
+﻿//task 25 
+
+/*
+double exponentingNum(int num1, int num2)
+{
+    return Math.Pow((num1),num2);
+}
+
+int num1;
+Console.WriteLine("Enter first integer number");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2;
+Console.WriteLine("Enter second integer number");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(exponentingNum(num1,num2)); */
+
+/*
+void exponentingNum()
+{
+    int num1;
+    Console.WriteLine("Enter first integer number");
+    num1 = Convert.ToInt32(Console.ReadLine());
+
+    int num2;
+    Console.WriteLine("Enter second integer number");
+    num2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Math.Pow((num1),num2));
+
+}
+
+exponentingNum();
+
+*/
+
+//task 29
+
+int createArray()
+{
+    
+}
